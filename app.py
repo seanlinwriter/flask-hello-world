1af2565e7ca74423cb65f6eb13c94cf317be12bd
Ching-Hsiang Lin <sean.lin.writer@gmail.com>
2026-07-28 16:44:25 +0000

    Add db_test route

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+import psycopg2
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World from Ching-Hsiang Lin in CSPB 3308!'
+
+@app.route("db_test")
+def db_test():
+    conn = None
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        return "Database connection successful"
+    except Exception as e:
+        return f"Database connection failed: {e}"
+    finally:
+        if conn is not None:
+            conn.close()
