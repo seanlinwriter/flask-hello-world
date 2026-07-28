b1d5acdf7a49a66709e39e4cbbe48f9843bf21e0
Ching-Hsiang Lin <sean.lin.writer@gmail.com>
2026-07-28 16:57:59 +0000

    Add db url

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask
 import psycopg2
 app = Flask(__name__)
 
-DATABASE_URL = "postgresql://sean_postgres_db_user:3mGXzlo9Ga1fAUrMU20BBDcSH1hlKsvP@dpg-d9kdkatg1s2s73fth090-a/sean_postgres_db"
-
 @app.route('/')
 def index():
     return 'Hello, World from Ching-Hsiang Lin in CSPB 3308!'
@@ -12,7 +10,7 @@ def index():
 def db_test():
     conn = None
     try:
-        conn = psycopg2.connect(DATABASE_URL)
+        conn = psycopg2.connect("postgresql://sean_postgres_db_user:3mGXzlo9Ga1fAUrMU20BBDcSH1hlKsvP@dpg-d9kdkatg1s2s73fth090-a/sean_postgres_db")
         return "Database connection successful"
     except Exception as e:
         return f"Database connection failed: {e}"
